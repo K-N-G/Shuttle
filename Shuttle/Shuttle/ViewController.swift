7658765170b66811fdc8282173235cfa53bdce79
Nikolay Kovachev <rrr_ood@yahoo.com>
2020-04-26 14:36:43 +0300

    add web view

--- Shuttle/Shuttle/ViewController.swift
+++ Shuttle/Shuttle/ViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadSite()
         // Do any additional setup after loading the view.
+    }
+    
+    func loadSite() {
+        var myURL: URL!
+        myURL = URL(string: "https://shuttle.bg")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
     }
 
 
